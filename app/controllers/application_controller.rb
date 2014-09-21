3ef4e7d5e33e950c2729cacdc6da864200dd7a14
Kushal <kushal.nigam1@gmail.com>
2014-09-21 08:54:53 -0400

    fixed signing up as a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  before_filter :configure_devise_params, if: :devise_controller?
+  
+def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone_no, :email_notifications, :text_notifications)
+    end
+  end
 end
