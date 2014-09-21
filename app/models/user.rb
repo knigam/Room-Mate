16a45c1bfa54a9a702b484630cd638912b26e27a
Trey Washington <lt.washington9@gmail.com>
2014-09-21 05:28:58 -0400

    send sms and send email methods for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,50 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :items
   has_and_belongs_to_many :tasks
+
+  def notify(task)
+  	if self.email_notifications
+  		send_email(task)
+  	end
+  	if self.text_notifications
+  		send_sms(task)
+  	end
+  end
+
+  private
+  def send_sms(task)
+  	account_sid = 'ACd9c82ecb10bf6bfc3a4572aed443f882'
+	auth_token = '{{ b2038599737574dd64f3efbbb4187679 }}'
+	@client = Twilio::REST::Client.new account_sid, auth_token
+ 
+	message = @client.account.messages.create(
+		:body => "It is your turn for" + task,
+    	:to => self.phone_no,
+    	:from => "+16784363595",
+    )
+	puts message.to
+
+  end
+
+  private
+  def send_email(task)
+  	m = Mandrill::API.new '1bIbtiJDlLgJjqw4YuPegw'
+  	body = "It\'s your turn Room Mate!"
+	message = {  
+	 :subject=> body,  
+	 :from_name=> "Room Mate",  
+	 :text=>"It is your turn for" + task,  
+	 :to=>[  
+	   {  
+	     :email=> self.email,  
+	     :name=> self.first_name  
+	   }  
+	 ],  
+	 :html=>"<html> #{body} </html>",  
+	 :from_email=>"roommate@noreply.com"  
+	}  
+	sending = m.messages.send message  
+	puts sending
+
+  end
 end
