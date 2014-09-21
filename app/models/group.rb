112eb8cfb79222b4183bec8f1d9a66aa36cc3da2
Kushal <kushal.nigam1@gmail.com>
2014-09-21 02:22:43 -0400

    added recursive destroy on groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  has_many :items
-  has_many :tasks
+  has_many :items, :dependent => :destroy
+  has_many :tasks, :dependent => :destroy
   has_and_belongs_to_many :users
 end
